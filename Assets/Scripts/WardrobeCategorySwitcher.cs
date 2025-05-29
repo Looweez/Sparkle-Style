d15145eb319fe1e2b24121650e9d059ae7e821ec
Ceili <ST10438681@vgconnect.edu.za>
2025-05-29 10:15:47 +0200

    added wardrobe stuff and audio stuff

--- Assets/Scripts/WardrobeCategorySwitcher.cs
+++ Assets/Scripts/WardrobeCategorySwitcher.cs
@@ -7,11 +7,21 @@ public class WardrobeCategorySwitcher : MonoBehaviour
     public GameObject topsPanel;
     public GameObject bottomsPanel;
     public GameObject shoesPanel;
+    public GameObject socksPanel;
+    public GameObject fullbodyPanel;
+    public GameObject outerwearPanel;
+    public GameObject hairPanel;
+    public GameObject accessoriesPanel;
 
     public void ShowCategory(string category)
     {
         topsPanel.SetActive(category == "Tops");
         bottomsPanel.SetActive(category == "Bottoms");
         shoesPanel.SetActive(category == "Shoes");
+        socksPanel.SetActive(category == "Socks");
+        fullbodyPanel.SetActive(category == "Fullbody");
+        outerwearPanel.SetActive(category == "Outerwear");
+        hairPanel.SetActive(category == "Hair");
+        accessoriesPanel.SetActive(category == "Accessories");
     }
 }
