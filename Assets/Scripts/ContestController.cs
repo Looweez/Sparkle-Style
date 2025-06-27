ae15dbf12aedf53089912878ee3e1a1f8b24bf6f
Looweez <59008362+Looweez@users.noreply.github.com>
2025-06-27 08:57:22 +0200

    backgrounds and icons

--- Assets/Scripts/ContestController.cs
+++ Assets/Scripts/ContestController.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ContestController : MonoBehaviour
 {
     // contest ui and selection
     
     public ClothingContest contestToLoad;
+    [SerializeField] private Button exitComputerButton;
+
+    void Start()
+    {
+        exitComputerButton.onClick.AddListener(OnExitComputerClick);
+    }
 
     public void OnClick()
     {
         ContestDataCarrier.instance.selectedContest = contestToLoad;
         SceneManager.LoadScene("Contest");
     }
+    
+    public void OnExitComputerClick()
+    {
+        SceneManager.LoadScene("Bedroom");
+    }
+            
 }
